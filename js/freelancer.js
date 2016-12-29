7a156a92faf8590ef6b288cae2a2930e93107770
Pablo Jurado <pjurado85@gmail.com>
2016-12-29 15:44:30 -0600

    fixed grid, redesign in process

--- js/freelancer.js
+++ js/freelancer.js
@@ -6,7 +6,7 @@
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 100)
+            scrollTop: ($($anchor.attr('href')).offset().top)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -14,7 +14,7 @@
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: 51
+        offset: 1
     });
 
     // Closes the Responsive Menu on Menu Item Click
